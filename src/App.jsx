6e8aff3c645cc9b6d8f7cdc8a4c9ca14c5d2d232
Fareed Javed <fareedjaved203@gmail.com>
2023-06-05 00:34:17 +0500

    Created Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { RxDragHandleDots2 } from "react-icons/rx";
-import { FaGripLines } from "react-icons/fa";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { IoIosArrowUp } from "react-icons/io";
-import { BsCheckCircleFill } from "react-icons/bs";
-import { BsCircle } from "react-icons/bs";
 import axios from "axios";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -33,13 +27,13 @@ const App = () => {
           const showPending = response.data.filter((val) => {
             return val.status === "pending";
           });
-          console.log(`pending: ${showPending}`);
+
           return setTodos(showPending);
         } else {
           const showCompleted = response.data.filter((val) => {
             return val.status === "completed";
           });
-          console.log(`completed: ${showCompleted}`);
+
           return setTodos(showCompleted);
         }
       } catch (error) {
@@ -111,113 +105,41 @@ const App = () => {
       console.log(error);
     }
   };
-  const formattedDate = (date) => {
-    const doIt = date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    });
-    return doIt;
-  };
+
   return (
     <>
       <div className="container-fluid">
         <div className="todo-structure">
-          <div className="item-adder">
-            <Container>
-              <Row>
-                <Col>
-                  <FaGripLines
-                    size={30}
-                    style={{ color: "white" }}
-                    onClick={toggleMode}
-                  />
-                </Col>
-                <Col xs={8}>
-                  {mode === "input" && (
-                    <input
-                      placeholder="To do list"
-                      type="text"
-                      value={inputValue}
-                      onKeyPress={handleKeyPress}
-                      onChange={handleChange}
-                    />
-                  )}
-                  {mode === "completed" && <p>Completed Tasks</p>}
-                  {mode === "pending" && <p>Pending Tasks</p>}
-                </Col>
-                <Col>
-                  {accordianChecked ? (
-                    <IoIosArrowUp
-                      size={30}
-                      style={{ color: "white" }}
-                      onClick={toggleVisibility}
-                    />
-                  ) : (
-                    <MdKeyboardArrowDown
-                      size={30}
-                      style={{ color: "white" }}
-                      onClick={toggleVisibility}
-                    />
-                  )}
-                </Col>
-              </Row>
-            </Container>
-          </div>
-          <div className="item-display">
-            {isHidden ? null : (
-              <ul>
-                {todos.map((value) => (
-                  <li
-                    key={value._id}
-                    onClick={() => toggleAccordion(value._id)}
-                  >
-                    <Container>
-                      <Row>
-                        <Col>
-                          {value.status === "completed" ? (
-                            <BsCheckCircleFill
-                              size={20}
-                              style={{ color: "black" }}
-                            />
-                          ) : (
-                            <BsCircle
-                              onClick={() => handleIconToggle(value._id)}
-                              size={20}
-                              style={{ color: "black" }}
-                            />
-                          )}
-                        </Col>
-                        <Col xs={8}>{value.task}</Col>
-                        <Col>
-                          <RxDragHandleDots2
-                            size={20}
-                            style={{ color: "black" }}
-                            onClick={() => removeTodo(value._id)}
-                          />
-                        </Col>
-                      </Row>
-                      {isOpen && confirmId === value._id && (
-                        <Container>
-                          <Row>
-                            <b>Start Time:</b> {value.startTime}
-                          </Row>
-                          <Row>
-                            <b>Completion Time:</b>
-                            {value.completionTime}
-                          </Row>
-                          <Row>
-                            <b>Status:</b> {value.status}
-                          </Row>
-                        </Container>
-                      )}
-                    </Container>
-                    <hr />
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
+          <TodoInput
+            mode={mode}
+            toggleMode={toggleMode}
+            inputValue={inputValue}
+            handleKeyPress={handleKeyPress}
+            handleChange={handleChange}
+            isHidden={isHidden}
+            toggleVisibility={toggleVisibility}
+          />
+          <TodoList
+            todos={todos}
+            toggleAccordion={toggleAccordion}
+            handleIconToggle={handleIconToggle}
+            removeTodo={removeTodo}
+            isHidden={isHidden}
+            isOpen={isOpen}
+            confirmId={confirmId}
+          >
+            {todos.map((value) => (
+              <TodoItem
+                key={value._id}
+                todo={value}
+                toggleAccordion={toggleAccordion}
+                handleIconToggle={handleIconToggle}
+                removeTodo={removeTodo}
+                isOpen={isOpen}
+                confirmId={confirmId}
+              />
+            ))}
+          </TodoList>
         </div>
         <footer>&copy; 2023, Muhammad Fareed Javed. All Rights Reserved</footer>
       </div>
